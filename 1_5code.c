0f6fb6e5f72457dd550b8065e2fcfeff7874f328
Kazusa613732 <aa20011204@gmail.com>
2023-08-17 22:03:12 +0800

    1-5執行緒

---
+++ 1_5code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <process.h>
+
+UINT __stdcall staff(PVOID lp){
+
+    printf("The num is %d\n", GetCurrentThreadId());
+    return 0;
+}
+
+int main(){
+    HANDLE h0 = (HANDLE)_beginthreadex(NULL, 0 ,staff, NULL, 0,NULL);
+    WaitForSingleObject(h0, INFINITE);
+    CloseHandle(h0);
+}
