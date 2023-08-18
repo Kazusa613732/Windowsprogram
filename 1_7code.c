aa89e810833dd0eefc4bf33b00960fe88047b4b2
Kazusa613732 <aa20011204@gmail.com>
2023-08-18 20:48:04 +0800

    1_7code 多執行續加上計算時間功能

--- 1_7code.c
+++ 1_7code.c
@@ -3,6 +3,8 @@
 #include <Windows.h>
 #include <process.h>
 
+#include <time.h>
+
 UINT __stdcall staff(PVOID lp){
     int count_num = *(LPINT) lp;
     printf("The num is %d , ³ø¤W¼Æ¦r %d.\n", GetCurrentThreadId(),count_num);
@@ -10,7 +12,9 @@ UINT __stdcall staff(PVOID lp){
 }
 
 int main(){
-    const int staff_num = 11;
+    double START,END;
+	START = clock();
+    const int staff_num = 10000;
     HANDLE hd[staff_num];
     int count_num[staff_num];
     int num = 0;
@@ -18,10 +22,15 @@ int main(){
         count_num[i] = i + 1;
         hd[i] = (HANDLE)_beginthreadex(NULL, 0 ,staff, &count_num[i], 0,NULL);
     }
-    
+       
     WaitForMultipleObjects(staff_num , hd , TRUE ,INFINITE);
     for(int i = 0; i < staff_num; i++){
         CloseHandle(hd[i]);
     }
+
+    END = clock();
+    printf("Time = %f\n==========================", (double)clock()/CLOCKS_PER_SEC);
+    printf("Time2 = %f\n========================= \n", (END - START) / CLOCKS_PER_SEC);
+    
     
 }
